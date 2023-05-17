b58aedca5e30990d4e305635d93d5b32352c7023
Kamil Kruk <kamil@Kamils-MacBook-Pro.local>
2023-05-18 00:24:03 +0200

    center content in welcome screens and reintend files with 2 spaces

--- src/sceens/WelcomeScreen.js
+++ src/sceens/WelcomeScreen.js
@@ -3,41 +3,44 @@ import AppButton from '../components/AppButton';
 import AppText from "../components/AppText";
 import { useNavigation } from '@react-navigation/native';
 import CenteredContainer from "../components/CenteredContainer";
+import FullHeightContainer from "../components/FullHeightContainer";
 
 const WelcomeScreen = () =>{
-    const navigation = useNavigation();
-    const [text, setText] = useState('');
-    const fullText = 'Welcome to securio, the gamified security awareness application. Remember to keep your user credentials to yourself!';
-    const [isTextComplete, setIsTextComplete] = useState(false);
-    useEffect(() => {
-        let currentText = '';
-        let index = 0;
+  const navigation = useNavigation();
+  const [text, setText] = useState('');
+  const fullText = 'Welcome to securio, the gamified security awareness application. Remember to keep your user credentials to yourself!';
+  const [isTextComplete, setIsTextComplete] = useState(false);
+  useEffect(() => {
+    let currentText = '';
+    let index = 0;
 
-        const timer = setInterval(() => {
-            currentText += fullText.charAt(index);
-            index++;
+    const timer = setInterval(() => {
+      currentText += fullText.charAt(index);
+      index++;
 
-            setText(currentText);
+      setText(currentText);
 
-            if (index === fullText.length) {
-                clearInterval(timer);
-                setIsTextComplete(true);
-            }
-        }, 1);
+      if (index === fullText.length) {
+        clearInterval(timer);
+        setIsTextComplete(true);
+      }
+    }, 1);
 
-        return () => {
-            clearInterval(timer);
-        };
-    }, []);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
 
-    return(
+  return(
+    <FullHeightContainer>
       <CenteredContainer>
         <AppText>{text}</AppText>
-            {isTextComplete && (
-                <AppButton title="Continue" onPress={()=> navigation.navigate('LoginScreen')} />
-            )}
-        </CenteredContainer>
-    );
+        {isTextComplete && (
+          <AppButton title="Continue" onPress={()=> navigation.navigate('LoginScreen')} />
+        )}
+      </CenteredContainer>
+    </FullHeightContainer>
+  );
 };
 
 export default WelcomeScreen;
