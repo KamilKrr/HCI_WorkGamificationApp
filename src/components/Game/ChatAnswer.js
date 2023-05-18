3c30ea765840d163473b05ce400ff21c5f2475e3
Kamil Kruk <kamil@Kamils-MacBook-Pro.local>
2023-05-18 14:55:23 +0200

    improve dark mode support for game screen

--- src/components/Game/ChatAnswer.js
+++ src/components/Game/ChatAnswer.js
@@ -19,7 +19,7 @@ const ChatQuestion = ({ person, options, sendAnswer, answer }) => {
         <View className="flex items-center justify-center h-10 w-10 rounded-full bg-blue-700 flex-shrink-0">
           <Text className={"text-white"}>{person}</Text>
         </View>
-        <View className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl w-10/12">
+        <View className="relative ml-3 text-sm bg-white dark:bg-gray-800 py-2 px-4 shadow rounded-xl w-10/12">
           <AnswerOptionPicker options={options} hasSendAnswer={hasSendAnswer} correctAnswer={answer} selectedAnswer={selectedAnswer} setAnswer={setSelectedAnswer}/>
 
           {(selectedAnswer !== -1 && !hasSendAnswer)? <TouchableOpacity onPress={handeSendAnswer} className={"mb-1 flex flex-row items-center justify-center py-2 bg-blue-600 rounded-lg dark:bg-blue-600"}>
--- src/sceens/EvalScreen.js
+++ src/sceens/EvalScreen.js
@@ -2,18 +2,19 @@ import React, {useState} from 'react';
 import FullHeightContainer from "../components/FullHeightContainer";
 import CenteredContainer from "../components/CenteredContainer";
 import {View, Text} from "react-native";
+import AppText from "../components/AppText";
 const EvalScreen = ({route, navigation}) => {
   const { courseID, difficulty, duration, sessionXP, correctAnswers, wrongAnswers } = route.params;
 
   return (
     <FullHeightContainer classes={"px-3"}>
       <CenteredContainer>
-        <Text>CourseID {courseID}</Text>
-        <Text>Gained Session XP (can be negative) {sessionXP}</Text>
-        <Text>Selected game duration (0-60min) {duration}</Text>
-        <Text>Selected game difficulty (0-2) {difficulty}</Text>
-        <Text>correct Answers {correctAnswers}</Text>
-        <Text>wrong Answers {wrongAnswers}</Text>
+        <AppText>CourseID {courseID}</AppText>
+        <AppText>Gained Session XP (can be negative) {sessionXP}</AppText>
+        <AppText>Selected game duration (0-60min) {duration}</AppText>
+        <AppText>Selected game difficulty (0-2) {difficulty}</AppText>
+        <AppText>correct Answers {correctAnswers}</AppText>
+        <AppText>wrong Answers {wrongAnswers}</AppText>
       </CenteredContainer>
     </FullHeightContainer>
   )
