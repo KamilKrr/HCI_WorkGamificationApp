b1707a8c880cb8f1e6a722447285acbacb3805da
Kamil Kruk <kamil@Kamils-MacBook-Pro.local>
2023-05-22 16:03:15 +0200

    add veritcal scrolling to course selection screen

--- src/sceens/HomeScreen.js
+++ src/sceens/HomeScreen.js
@@ -6,17 +6,21 @@ import AppSubText from "../components/AppSubText";
 import Course from "../components/Course";
 import courses from "../../assets/courses";
 import {CourseList} from "../components/CourseList";
+import {ScrollView} from "nativewind/dist/preflight";
 const HomeScreen = ({navigation}) => {
   return (
     <Container>
-      <AppText>Gamification App</AppText>
+      <AppText classes={"text-lg"}>Gamification App</AppText>
       <AppSubText>HCI Aufgabe 3, Gruppe 1</AppSubText>
-
-      <CourseList navigation={navigation} heading={"Beginner Topics"} courses={courses.filter(c => c.category === "Beginner")}></CourseList>
-      <CourseList navigation={navigation} heading={"Intermediate Topics"} courses={courses.filter(c => c.category === "Intermediate")}></CourseList>
-      <CourseList navigation={navigation} heading={"Advanced Topics"} courses={courses.filter(c => c.category === "Expert")}></CourseList>
+      <ScrollView>
+        <View className={"pb-10"}>
+          <CourseList navigation={navigation} heading={"Beginner Topics"} courses={courses.filter(c => c.category === "Beginner")}></CourseList>
+          <CourseList navigation={navigation} heading={"Intermediate Topics"} courses={courses.filter(c => c.category === "Intermediate")}></CourseList>
+          <CourseList navigation={navigation} heading={"Advanced Topics"} courses={courses.filter(c => c.category === "Expert")}></CourseList>
+        </View>
+      </ScrollView>
     </Container>
-)
+  )
 }
 
 export default HomeScreen;
